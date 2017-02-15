1d5fdc08db5efaebca37499a68d707a3e5176a5d
Ida Huang <ihuangda@gmail.com>
2017-02-14 20:17:01 -0800

    pick_node changes

--- pick_nodes.py
+++ pick_nodes.py
@@ -1,16 +1,16 @@
 import networkx as nx
 import heapq
-
+import operator 
 
 def pick_seeds(in_graph, num_seeds):
     # Dictionary of nodes and betweenness values
-    betweenness_values = nx.betweenness_centrality()
+    betweenness_values = nx.betweenness_centrality(in_graph)
 
     list_betweenness = betweenness_values.items()   # Convert to tuple list
 
     # Get top betweenness ranked nodes and values
     top_between = heapq.nlargest(num_seeds, list_betweenness,
-                                 key=itemgetter(1))
+                                 key=operator.itemgetter(1))
 
     top_nodes = [tup[0] for tup in top_between]     # Get only node IDs
 
